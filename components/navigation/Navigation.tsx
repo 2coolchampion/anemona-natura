cce6de2306a1f28b5dd79aa15fb720194fb333ee
2coolchampion <franvidicek@gmail.com>
2024-01-03 21:56:43 +0100

    Make the navigation bar Scroll-Activated

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,12 +1,38 @@
-import React from "react"
+"use client"
+
+import React, { useEffect, useRef, useState } from "react"
 import "./styles.css"
 
 const Nav = () => {
+  const [isHidden, setIsHidden] = useState(false)
+  const prevScrollPositionRef = useRef(0)
+
+  useEffect(() => {
+    function handleScroll() {
+      const currentScrollPosition = window.scrollY
+      setIsHidden(prevScrollPositionRef.current < currentScrollPosition)
+      prevScrollPositionRef.current = currentScrollPosition
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  const logHidden = () => {
+    console.log(isHidden)
+  }
+
   return (
-    <div className="nav flex h-16 w-full justify-between bg-indigo-500 p-2 text-white">
+    <nav
+      className={`nav fixed z-10 flex h-16 w-full justify-between bg-indigo-800 p-2 transition-transform ${
+        isHidden ? "-translate-y-full" : "translate-y-0"
+      }`}
+    >
       <div className="h-full w-[180px] bg-indigo-200"></div>
       <div className="h-full w-[50px] bg-indigo-200"></div>
-    </div>
+    </nav>
   )
 }
 
