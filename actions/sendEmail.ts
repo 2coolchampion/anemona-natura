fc571b64b6e670a244278731a8666df27ec8894e
2coolchampion <franvidicek@gmail.com>
2024-01-17 19:58:26 +0100

    Extract data from formData parameter Return if there is no message Modify the subject and text copy and add the reply_to parameter

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -2,12 +2,24 @@ import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-const sendEmail = async (formData: any) => {
+const sendEmail = async (formData: FormData) => {
+  const email = formData.get("email")
+  const ime = formData.get("ime")
+  const kontaktTel = formData.get("kontaktTel")
+  const poruka = formData.get("poruka")
+
+  if (!poruka) {
+    return {
+      error: "Poruka je obavezna!",
+    }
+  }
+
   const data = await resend.emails.send({
     from: "Acme <onboarding@resend.dev>",
     to: "franvidicek@gmail.com",
-    subject: `📬 Nova poruka - ${formData.ime}`,
-    text: "Hello World!",
+    subject: `📬 Nova poruka - ${ime} - ${kontaktTel}`,
+    reply_to: email as string,
+    text: `Poruka: ${poruka} || Email: ${email}`,
   })
 
   console.log(formData)
