ce4f5f4480f4e000aef65bf569fe5415dd77659b
2coolchampion <franvidicek@gmail.com>
2024-01-17 02:29:29 +0100

    Make the phone number and email adress open the phone or mail app Make the text backgronud-light color instead of white

--- components/KontaktSection.tsx
+++ components/KontaktSection.tsx
@@ -25,23 +25,27 @@ const KontaktSection = ({ className }: KontaktSectionProps) => {
           <div className="flex  w-full items-center gap-4 p-4 sm:px-0">
             <PersonIcon className="h-8 w-8 text-background-light" />
 
-            <div className="text-white">
+            <div className="text-background-light">
               Vlasnica <br /> Sabrina Hribar
             </div>
           </div>
           <div className="flex w-full items-center gap-4 p-4 sm:px-0">
             <PinIcon className="h-9 text-background-light" />
-            <div className="text-white">
+            <div className="text-background-light">
               Adreasa1 <br /> Adresa 2
             </div>
           </div>
           <div className="flex w-full items-center gap-4 p-4 sm:px-0">
             <PhoneIcon className="h-8 w-8 text-background-light" />
-            <div className="text-white underline">+385 99 286 8188</div>
+            <div className="text-background-light underline">
+              <a href="tel:%2B385992868188">+385 99 286 8188</a>
+            </div>
           </div>
-          <div className="flex w-full items-center gap-4 p-4 sm:px-0">
+          <div className="flex w-full items-center gap-4 p-4 text-background-light underline sm:px-0">
             <MailIcon className="w-8 text-background-light" />
-            <div className="text-white">anemora.natura.info@gmail.com</div>
+            <a href="mailto:anemona.natura.info@gmail.com">
+              anemona.natura.info@gmail.com
+            </a>
           </div>
         </div>
       </section>
