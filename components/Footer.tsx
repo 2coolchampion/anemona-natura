69390e4bec5bf70008546e0cbe32680fcada7092
2coolchampion <franvidicek@gmail.com>
2024-01-17 01:47:50 +0100

    Improve the default spacing of the footer elements

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from "react"
 const Footer = () => {
   return (
     <footer className="relative flex h-64 w-full flex-col justify-center bg-green-dark py-8 text-center text-xs text-white">
-      <div className="flex h-fit justify-center">
-        <div className="flex flex-col items-start border-r border-dashed px-9">
+      <div className="flex h-fit w-full justify-center">
+        <div className="border- flex flex-col items-start border-r border-dashed pr-32">
           <h4 className="text-sm font-medium">STRANICE</h4>
           <ul className="mt-2 flex flex-col flex-wrap items-start font-light leading-6">
             <li className="text-white">Naslovnica</li>
@@ -12,7 +12,7 @@ const Footer = () => {
             <li className="text-white">EU Projekti</li>
           </ul>
         </div>
-        <div className="flex flex-col items-start px-9">
+        <div className="flex flex-col items-start px-32">
           <h4 className="text-sm font-medium">KONTAKT</h4>
           <ul className="mt-2 flex flex-col flex-wrap items-start font-light leading-6">
             <li className="text-white">VL. Sabrina Hribar</li>
@@ -20,7 +20,7 @@ const Footer = () => {
             <li className="text-white">anemona.natura.info@gmail.com</li>
           </ul>
         </div>
-        <div className="flex flex-col items-start px-9">
+        <div className="flex flex-col items-start ">
           <h4 className="text-sm font-medium">ADRESA</h4>
           <ul className="flex-wrapfont-light mt-2 flex flex-col  items-start leading-6">
             <li className="text-white">Anomna NAture d.o.o.</li>
@@ -29,7 +29,7 @@ const Footer = () => {
           </ul>
         </div>
       </div>
-      <p className="absolute bottom-8 left-0 right-0 mx-auto text-center">
+      <p className="absolute bottom-5 left-0 right-0 mx-auto text-center">
         © {new Date().getFullYear()} Anemora Natura d.o.o.
       </p>
     </footer>
