6cc29d690df5d9c07116b6e253c37a661c13dde8
2coolchampion <franvidicek@gmail.com>
2024-01-21 23:26:37 +0100

    Add basic responsive navigation bar

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useRef, useState } from "react"
 import "./styles.css"
-import { HamMenuIcon } from "@/public/icons"
+import { HamMenuIcon, MailIcon, PhoneIcon } from "@/public/icons"
 import { XIcon } from "@/public/icons"
 import Logo from "@/public/logo.svg"
 import NavLinks from "./navLinks"
@@ -16,6 +16,9 @@ const Nav = () => {
     function handleScroll() {
       const currentScrollPosition = window.scrollY
       setIsHidden(prevScrollPositionRef.current < currentScrollPosition)
+      setIsOpened(
+        prevScrollPositionRef.current < currentScrollPosition ? false : isOpened
+      )
       prevScrollPositionRef.current = currentScrollPosition
     }
 
@@ -30,29 +33,67 @@ const Nav = () => {
   }
 
   return (
-    <nav
-      className={`nav fixed z-10 flex h-16 w-full justify-between bg-background-light p-2 px-4 drop-shadow-lg transition-transform ${
-        isHidden ? "-translate-y-full" : "translate-y-0"
-      }`}
-    >
-      <div className="flex items-center justify-center">
-        <Logo className="w-40" />
-      </div>
-      <NavLinks />
-      <div className="flex items-center justify-center">
-        {isOpened ? (
-          <XIcon
-            onClick={() => setIsOpened(false)}
-            className="w-8 text-green-dark"
-          />
-        ) : (
-          <HamMenuIcon
-            onClick={() => setIsOpened(true)}
-            className="w-10 text-green-dark"
-          />
-        )}
-      </div>
-    </nav>
+    <>
+      <header className="fixed top-0 z-20 w-full">
+        <nav
+          className={`nav flex h-16 w-full justify-between bg-background-light px-4 py-2 ${
+            isOpened ? "" : "shadow-lg"
+          } transition-transform ${
+            isHidden ? "-translate-y-full" : "translate-y-0"
+          }`}
+        >
+          {/* LOGO */}
+          <div className="flex items-center justify-center">
+            <Logo className="w-40" />
+          </div>
+          {/* NAVIGATION LINKS - MD */}
+          <div className="hidden md:flex md:flex-row">
+            <NavLinks />
+          </div>
+          {/* HAM MENU ICON */}
+          <button
+            onClick={() => setIsOpened(!isOpened)}
+            className="flex items-center justify-center md:hidden"
+          >
+            {isOpened ? (
+              <XIcon className="w-8 text-green-dark" />
+            ) : (
+              <HamMenuIcon className="w-10 text-green-dark" />
+            )}
+          </button>
+        </nav>
+        {/* NAVIGATION LINKS - MOBILE */}
+        <div
+          className={`drop-shadow-2xl transition-transform ${
+            isHidden ? "-translate-y-[27rem]" : "translate-y-0"
+          } ${
+            isOpened ? "flex" : "hidden"
+          } flex-col bg-background-light md:hidden`}
+        >
+          <NavLinks />
+          <div className="mt-8 flex flex-col bg-green-light px-2 py-3 text-lg">
+            <div className="flex items-center px-2 py-3">
+              <PhoneIcon className="mr-2 inline-block h-7 w-7 fill-current text-background-light" />
+              <span className="text-background-light">099 286 8118</span>
+            </div>
+            <div className="flex items-center overflow-x-auto px-2 py-3">
+              <MailIcon className="mr-2 inline-block h-7 w-7 flex-none fill-current text-background-light" />
+              <span className=" text-base text-background-light">
+                anemona.natura.info@gmail.com
+              </span>
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* Backdrop */}
+      <div
+        className={`fixed inset-0 z-10 bg-black transition-opacity duration-1000 ease-out ${
+          isOpened && !isHidden ? "opacity-50" : "opacity-0"
+        }`}
+        style={{ pointerEvents: isOpened ? "auto" : "none" }}
+        onClick={() => setIsOpened(false)}
+      ></div>
+    </>
   )
 }
 
--- components/navigation/navLinks.tsx
+++ components/navigation/navLinks.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const navLinks = () => {
   return (
-    <ul className="hidden gap-6 md:flex">
+    <ul className="text-md mt-8 flex flex-col gap-4 px-4 py-2 text-3xl md:m-0 md:flex-row md:items-center md:gap-6 md:p-0 md:text-base ">
       <li>Naslovnica</li>
       <li>Usluge</li>
       <li>EU Projekti</li>
