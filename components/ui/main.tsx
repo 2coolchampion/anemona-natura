d0b526d8ef8d1c3a9cded6dfdb7b02f24ad7b869
2coolchampion <franvidicek@gmail.com>
2024-01-22 18:30:30 +0100

    Make the min height of MainWrapper min-h-screen and jsutify it's items to the start

--- components/ui/main.tsx
+++ components/ui/main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const MainWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex w-screen flex-col justify-center bg-background-light xl:max-w-[80vw] 2xl:w-4/5 2xl:max-w-[1535px]">
+    <main className="flex min-h-screen w-screen flex-col justify-start bg-background-light xl:max-w-[80vw] 2xl:w-4/5 2xl:max-w-[1535px]">
       {children}
     </main>
   )
