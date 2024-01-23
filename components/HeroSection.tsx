1ba7e07948c78b55712e54e3247c5ceb65411edc
2coolchampion <franvidicek@gmail.com>
2024-01-23 20:50:06 +0100

    Make the hero section flex and center it's items by default Limit the max width of hero title on md

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,7 +5,7 @@ import KontaktFormaLG from "./KontaktFormaLG"
 
 const HeroSection = () => {
   return (
-    <section className="relative z-0 h-[66vh] max-h-[650px] w-full bg-green-dark px-4 sm:px-16 md:px-32  lg:max-h-[750px] 2xl:max-w-[1535px] landscape:h-[100vh]">
+    <section className="relative z-0 flex h-[66vh] max-h-[650px] w-full flex-col items-center bg-green-dark px-4 sm:px-16 md:px-32 lg:max-h-[750px] 2xl:max-w-[1535px] landscape:h-[100vh]">
       <Image
         src={HeroImage}
         alt="hero image"
@@ -14,7 +14,7 @@ const HeroSection = () => {
         priority
       />
       <div className="flex flex-col pt-56 lg:items-center lg:pt-48">
-        <h1 className="header-text text-4xl font-extrabold text-background-light lg:max-w-[45rem] lg:text-center"></h1>
+        <h1 className="header-text text-4xl font-extrabold text-background-light md:max-w-[26rem] lg:max-w-[45rem] lg:text-center"></h1>
         <a href="#usluge">
           <button className="mt-8 flex w-fit items-stretch bg-green-dark text-background-light">
             <div className="flex items-end bg-green-light px-3 py-2">
