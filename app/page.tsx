5442f7dd59c340da65d7ecf8480467b24c35e5aa
2coolchampion <franvidicek@gmail.com>
2024-01-23 10:36:16 +0100

    Make the heading background dark green so that it's not purple while the image is loading Same for UslugeCards

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <>
       <MainWrapper>
-        <section className="relative z-0 h-[66vh] max-h-[650px] w-full bg-indigo-400 px-4 sm:px-16  md:px-32 2xl:max-w-[1535px] landscape:h-[100vh]">
+        <section className="relative z-0 h-[66vh] max-h-[650px] w-full bg-green-dark px-4 sm:px-16  md:px-32 2xl:max-w-[1535px] landscape:h-[100vh]">
           <Image
             src={HeroImage}
             alt="hero image"
--- components/UslugeCard.tsx
+++ components/UslugeCard.tsx
@@ -18,7 +18,7 @@ const UslugeCard = ({
 }: UslugeCardProps) => {
   return (
     <div
-      className={`relative z-0 flex h-56 flex-col items-center justify-around overflow-hidden rounded-lg bg-black/70 text-center text-gray-100 xl:h-56 2xl:h-72 ${
+      className={`relative z-0 flex h-56 flex-col items-center justify-around overflow-hidden rounded-lg bg-green-dark text-center text-gray-100 xl:h-56 2xl:h-72 ${
         cardType === "small" ? "col-span-1" : "col-span-1 lg:col-span-2"
       }`}
     >
