22bd3a17d4365f1b02564bf513ac6ee653a285c2
2coolchampion <franvidicek@gmail.com>
2024-01-23 17:36:50 +0100

    Style: Make the KontaktFormaLG a bit less transparent and have a dark gradient

--- components/KontaktFormaLG.tsx
+++ components/KontaktFormaLG.tsx
@@ -3,7 +3,7 @@ import KontaktFromaV2 from "./KontaktFormaV2"
 
 const KontaktFormaLG = () => {
   return (
-    <div className="-mx-16 mt-28 hidden rounded-lg bg-white/35 pb-8 backdrop-blur-md lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr]">
+    <div className="-mx-16 mt-28 hidden rounded-lg bg-gradient-to-t from-green-dark/35  to-black/15 pb-8 backdrop-blur-xl lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr]">
       <div className="col-span-2 flex items-center justify-center">Kontakt</div>
 
       <div className="flex justify-end px-8">
