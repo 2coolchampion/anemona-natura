25d4314f06bae2d4830c2697ec196ed0514fe903
2coolchampion <franvidicek@gmail.com>
2024-01-23 20:28:00 +0100

    Style: KontaktFormaLG add styling to H2

--- components/KontaktFormaLG.tsx
+++ components/KontaktFormaLG.tsx
@@ -3,8 +3,12 @@ import KontaktFromaV2 from "./KontaktFormaV2"
 
 const KontaktFormaLG = () => {
   return (
-    <div className="-mx-16 mt-28 hidden rounded-lg bg-gradient-to-t from-green-dark/35  to-black/15 pb-8 backdrop-blur-xl lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr]">
-      <div className="col-span-2 flex items-center justify-center">Kontakt</div>
+    <div className="-mx-16 mt-56 hidden rounded-lg bg-gradient-to-t from-green-dark/45  to-white/5 pb-16 backdrop-blur-xl lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr]">
+      <div className="col-span-2 flex items-start justify-center">
+        <div className="mt-5 rounded-md  bg-green-dark px-4 py-1 text-lg font-semibold tracking-wide text-background-light">
+          <h2>KONTAKT</h2>
+        </div>
+      </div>
 
       <div className="flex justify-end px-8">
         <div className="w-96">
