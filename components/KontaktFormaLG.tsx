ea3354b91ed93375b57031d9e8b5491df0ad00ec
2coolchampion <franvidicek@gmail.com>
2024-01-23 15:38:35 +0100

    Style: Add a white transparent background to hero KontaktFormaLG

--- components/KontaktFormaLG.tsx
+++ components/KontaktFormaLG.tsx
@@ -1,21 +1,18 @@
 import KontaktAdresa from "./KontaktAdresa"
-import KontaktFroma from "./KontaktForma"
 
 const KontaktFormaLG = () => {
   return (
-    <div className="-mx-16 mt-28 hidden lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr]">
-      <div className="col-span-2 flex items-center justify-center bg-green-800">
-        Kontakt
-      </div>
+    <div className="-mx-16 mt-28 hidden rounded-lg bg-white/35 pb-8 backdrop-blur-md lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr]">
+      <div className="col-span-2 flex items-center justify-center">Kontakt</div>
 
-      <div className="flex justify-end  bg-green-300 p-8">
-        <div className="w-96 bg-green-400">
+      <div className="flex justify-end px-8">
+        <div className="w-96">
           <KontaktFroma />
         </div>
       </div>
 
-      <div className="flex bg-green-300 p-8">
-        <div className="flex w-96 justify-center bg-green-600">
+      <div className="flex px-8">
+        <div className="flex w-96 justify-center rounded-md bg-green-dark">
           <KontaktAdresa />
         </div>
       </div>
