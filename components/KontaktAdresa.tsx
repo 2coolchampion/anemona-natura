f4168a7f958edb2d7938bac2107057544d6021b4
2coolchampion <franvidicek@gmail.com>
2024-01-24 16:24:23 +0100

    Style: KontaktAdresa on lg and above

--- components/KontaktAdresa.tsx
+++ components/KontaktAdresa.tsx
@@ -2,30 +2,45 @@ import { PersonIcon, PhoneIcon, PinIcon, MailIcon } from "@/components/icons"
 
 const KontaktAdresa = () => {
   return (
-    <div className="mt-16 flex flex-col items-start justify-start space-y-4">
-      <div className="flex  w-full items-center gap-4 p-4 sm:px-0">
-        <PersonIcon className="h-8 w-8 text-background-light" />
+    <div className="mt-16 flex flex-col items-start justify-start space-y-4 text-background-light">
+      <div className="flex  w-full items-start gap-4 p-4 sm:px-0">
+        <PersonIcon className="lg:text-green-super-light h-8 w-8" />
 
-        <div className="text-background-light">VL. Sabrina Hribar</div>
+        <div className="lg:text-green-super-light lg:text-lg lg:font-bold">
+          VL. Sabrina Hribar
+        </div>
       </div>
-      <div className="flex w-full items-center gap-4 p-4 sm:px-0">
-        <PinIcon className="h-9 self-start text-background-light" />
-        <div className="text-background-light">
+      <div className="flex w-full items-start gap-4 p-4 sm:px-0">
+        <PinIcon className="lg:text-green-super-light h-9 self-start" />
+        <div>
+          <h3 className="text-green-super-light hidden text-lg font-semibold lg:block">
+            ADRESA
+          </h3>
           Anemona Natura d.o.o. <br /> M.J. Šporera 20 <br /> 47 000 Karlovac,
           Hrvatska
         </div>
       </div>
       <div className="flex w-full items-center gap-4 p-4 sm:px-0">
-        <PhoneIcon className="h-8 w-8 text-background-light" />
-        <div className="text-background-light underline">
-          <a href="tel:%2B385992868188">+385 99 286 8188</a>
+        <PhoneIcon className="lg:text-green-super-light h-8 w-8 self-start" />
+        <div className=" flex flex-col">
+          <h3 className="text-green-super-light hidden text-lg font-semibold lg:block">
+            BROJ TELEFONA
+          </h3>
+          <a href="tel:%2B385992868188" className="underline">
+            +385 99 286 8188
+          </a>
         </div>
       </div>
-      <div className="flex w-full items-center gap-4 p-4 text-background-light underline sm:px-0">
-        <MailIcon className="w-8 text-background-light" />
-        <a href="mailto:anemona.natura.info@gmail.com">
-          anemona.natura.info@gmail.com
-        </a>
+      <div className="flex w-full items-center gap-4 p-4 sm:px-0">
+        <MailIcon className="lg:text-green-super-light w-8 self-start" />
+        <div className="flex flex-col">
+          <h3 className="text-green-super-light hidden text-lg font-semibold lg:block">
+            EMAIL
+          </h3>
+          <a href="mailto:anemona.natura.info@gmail.com" className="underline">
+            anemona.natura.info@gmail.com
+          </a>
+        </div>
       </div>
     </div>
   )
