d5387c563b63f4e793e0dd3c197d1a88b6465a4c
2coolchampion <franvidicek@gmail.com>
2024-01-24 16:45:06 +0100

    Style: Tweak the background blobs on KontaktFormaLG

--- components/KontaktFormaLG.tsx
+++ components/KontaktFormaLG.tsx
@@ -9,13 +9,12 @@ import LeafBottom from "../public/blobs/leaf-bottom.svg"
 const KontaktFormaLG = () => {
   return (
     <div className="textured relative mt-56 hidden w-[65vw] min-w-[60rem] max-w-[71rem] overflow-clip rounded-lg border-2 border-white border-opacity-60 pb-16 backdrop-blur-xl before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-b before:from-white/55 before:to-white/35 before:content-[''] lg:grid lg:grid-cols-2 lg:grid-rows-[1.2fr_5fr]">
-      <BlobBrown className="absolute -right-8 -top-16 -z-10 opacity-50 blur-2xl" />
-      <div className="absolute -top-8 left-44 -z-10 h-24 w-[30rem] bg-gradient-radial from-orange-200/60 to-white/0 opacity-35 blur-2xl"></div>
+      <BlobBrown className="absolute -right-8 -top-16 -z-10 opacity-20 blur-2xl" />
       <BlobDark className="absolute -bottom-8 -left-36 -z-10 opacity-30 blur-2xl" />
-      <BlobGreen className="absolute -bottom-16 -right-64 -z-20 opacity-20" />
+      <BlobGreen className="absolute -bottom-16 -right-64 -z-20 opacity-15" />
       <BlobGreen className="after: absolute -bottom-16 -right-64 -z-20 opacity-70 blur-xl" />
       <Leaf className="absolute  -right-24 -top-16 -z-20  opacity-75" />
-      <LeafBottom className="absolute -bottom-[25rem] -left-[38rem] -z-20 opacity-20" />
+      <LeafBottom className="absolute -bottom-[25rem] -left-[38rem] -z-20 opacity-15" />
       <div className="col-span-2 flex items-center justify-center">
         <div className=" rounded-md  bg-green-dark px-4 py-2 text-lg font-semibold tracking-wide text-background-light">
           <h2>KONTAKT</h2>
