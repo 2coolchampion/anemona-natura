0038bf8b1f3872f6a6002fde09eb84f1dce85ece
2coolchampion <franvidicek@gmail.com>
2024-01-27 21:29:04 +0100

    Add underline animation to Link elements in the navbar
    fom md breakpoint navbar

--- components/navigation/navLinks.tsx
+++ components/navigation/navLinks.tsx
@@ -5,13 +5,22 @@ const navLinks = () => {
   return (
     <ul className="text-md mt-8 flex flex-col gap-4 px-4 py-2 text-3xl md:m-0 md:flex-row md:items-center md:gap-6 md:p-0 md:text-base ">
       <li>
-        <Link href="/">Naslovnica</Link>
+        <Link href="/" className="hover-underline-animation relative">
+          Naslovnica
+        </Link>
       </li>
       <li>
-        <Link href="/usluge">Usluge</Link>
+        <Link href="/usluge" className="hover-underline-animation relative">
+          Usluge
+        </Link>
       </li>
       <li>
-        <Link href="/eu-projekti">EU Projekti</Link>
+        <Link
+          href="/eu-projekti"
+          className="hover-underline-animation relative"
+        >
+          EU Projekti
+        </Link>
       </li>
     </ul>
   )
