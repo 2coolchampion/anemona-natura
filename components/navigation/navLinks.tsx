4000fbff78e182138af8ce407269d660ec3024b9
2coolchampion <franvidicek@gmail.com>
2024-01-28 18:31:48 +0100

    Make the currently active site menu item highlighted in navLinks

--- components/navigation/navLinks.tsx
+++ components/navigation/navLinks.tsx
@@ -1,27 +1,42 @@
 import React from "react"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const NavLinks = [
+  {
+    name: "Naslovnica",
+    href: "/",
+  },
+  {
+    name: "Usluge",
+    href: "/usluge",
+  },
+  {
+    name: "EU Projekti",
+    href: "/eu-projekti",
+  },
+]
 
 const navLinks = () => {
+  const pathname = usePathname()
+
   return (
     <ul className="text-md mt-8 flex flex-col gap-4 px-4 py-2 text-3xl md:m-0 md:flex-row md:items-center md:gap-6 md:p-0 md:text-base ">
-      <li>
-        <Link href="/" className="hover-underline-animation relative">
-          Naslovnica
-        </Link>
-      </li>
-      <li>
-        <Link href="/usluge" className="hover-underline-animation relative">
-          Usluge
-        </Link>
-      </li>
-      <li>
-        <Link
-          href="/eu-projekti"
-          className="hover-underline-animation relative"
-        >
-          EU Projekti
-        </Link>
-      </li>
+      {NavLinks.map((link) => {
+        return (
+          <li>
+            <Link
+              href={link.href}
+              key={link.name}
+              className={`hover-underline-animation relative ${
+                pathname.includes(link.href) ? "font-medium" : ""
+              }`}
+            >
+              {link.name}
+            </Link>
+          </li>
+        )
+      })}
     </ul>
   )
 }
