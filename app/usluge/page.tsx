aa8d90ca68d6ef1699294f82b3f3d554938ad4e1
2coolchampion <franvidicek@gmail.com>
2024-02-10 11:47:58 +0100

    Add copy to the Usluge page Style: Add basic styling to the Usluge page copy

--- app/usluge/page.tsx
+++ app/usluge/page.tsx
@@ -3,8 +3,72 @@ import MainWrapper from "@/components/ui/main"
 const Usluge = () => {
   return (
     <MainWrapper>
-      <section className="mt-48 text-center font-bold">
-        Usluge... Work in progress
+      <section className="my-44 px-11">
+        <h1 className="text-center text-3xl font-extrabold text-green-dark text-opacity-40">
+          Naše Usluge
+        </h1>
+        <h2 className="mt-8 text-lg font-medium text-green-dark">
+          Doznaka I Orprema
+        </h2>
+        <ul className="mt-2 list-disc">
+          <li>Doznaka stabala</li>
+          <li>Otprema drvnih sortimenata</li>
+        </ul>
+        <h2 className="mt-8 text-lg font-medium text-green-dark">
+          Lovnogospodarski Planovi
+        </h2>
+        <ul className="mt-2 list-disc">
+          <li>Lovnogospodarska osnova</li>
+          <li>Program uzgoja divljači</li>
+          <li>Program zaštite divljači</li>
+          <li>Revizija lovnogospodarskih planova</li>
+        </ul>
+        <h2 className="mt-8 text-lg font-medium text-green-dark">
+          Šumskogospodarski Planovi
+        </h2>
+        <ul className="mt-2 list-disc">
+          <li>Osnova gospodarenja gospodarskom jedinicom</li>
+          <li>
+            Program gospodarenja gospodarskom jedinicom s planom upravljanja
+            područjem ekološke mreže
+          </li>
+          <li>Program gospodarenja šumama privatnih posjednika</li>
+          <li>
+            Program gospodarenja šumama posebne namjene za potrebe obrane
+            Republike Hrvatske
+          </li>
+          <li>Program gospodarenja šumskim sjemenskim objektima</li>
+          <li>Program zaštite, njege i obnove šuma</li>
+          <li>
+            Program gospodarenja šumama posebne namjene kojima gospodari Pravna
+            osoba
+          </li>
+        </ul>
+        <h2 className="mt-8 text-lg font-medium text-green-dark">
+          Identifikacija katastarskih čestica
+        </h2>
+        <ul className="mt-2 list-disc">
+          <li>
+            Identifikacija katastarskih čestica geodetskim uređajem
+            registriranim u Državnoj geodetskoj upravi
+          </li>
+        </ul>
+        <h2>Procjena vrijdnosti šume i šumskog zemljišta</h2>
+        <ul className="mt-2 list-disc">
+          <li>Izrada elaborata procjene vrijednosti šumskih sastojina</li>
+          <li>Procjena vrijednosti šuma i šumskog zemljišta </li>
+          <li>Izračun vrijednosti drvne mase na panju</li>
+          <li>Izračun vrijednosti šumskih šteta</li>
+          <li>Izračun prihoda i rashoda u šumama</li>
+          <li>Izračun sječivih mogućnosti šumskih sastojina</li>
+          <li>Izračun vrijednosti pojedinačnih stabala</li>
+          <li>Izračun tržišne vrijednosti šumskog zemljišta</li>
+          <li>
+            Procjena vrijednosti šuma na trasama dalekovoda, vodovoda, prometne
+            i ostale infrastrukture
+          </li>
+          <li>Sve ostale usluge vještačenja u šumarstvu</li>
+        </ul>
       </section>
     </MainWrapper>
   )
