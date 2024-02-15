48aa8ab5a6e20029d218a84e68277f3e24b3cd48
2coolchampion <franvidicek@gmail.com>
2024-02-15 23:24:44 +0100

    style: lower border opacity of ToastSucess component

--- components/toast/ToastSuccess.tsx
+++ components/toast/ToastSuccess.tsx
@@ -2,7 +2,7 @@ import { Check } from "@/components/icons"
 
 const ToastSuccess = ({ message }: { message: string }) => {
   return (
-    <div className="flex items-center rounded-xl border-2 border-[#C7F9DA] bg-green-light-hover px-6 py-4 text-[#C7F9DA] shadow-xl">
+    <div className="flex items-center rounded-md border-2 border-[#C7F9DA] border-opacity-25 bg-green-light-hover px-6 py-4 text-[#C7F9DA] shadow-xl">
       {" "}
       <Check className="mr-2 w-5 " />
       {message}
