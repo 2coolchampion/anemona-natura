de94e992ce8b918db168b1821361abd622977f05
2coolchampion <franvidicek@gmail.com>
2024-02-18 13:38:28 +0100

    style: add sendIcon.riv to KontaktForma's submit btn

--- components/KontaktForma.tsx
+++ components/KontaktForma.tsx
@@ -5,9 +5,10 @@ import sendEmail from "@/actions/sendEmail"
 import { porukaSchema, Poruka } from "@/lib/types"
 import { useForm, SubmitHandler } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import { toast } from "sonner"
 import ToastSuccess from "./toast/ToastSuccess"
+import { useRive, useStateMachineInput } from "@rive-app/react-canvas-lite"
 
 const KontaktFroma = () => {
   const imeRef = useRef<HTMLInputElement | null>(null)
@@ -35,6 +36,14 @@ const KontaktFroma = () => {
   const { ref: emailRefRHF, ...emailRest } = register("email")
   const { ref: telRefRHF, ...telRest } = register("tel")
   const { ref: porukaRefRHF, ...porukaRest } = register("poruka")
+
+  useEffect(() => {
+    if (isFlyingInput) {
+      isSubmitting
+        ? (isFlyingInput.value = true)
+        : (isFlyingInput.value = false)
+    }
+  }, [isSubmitting])
 
   const clientSendEmailAPI: SubmitHandler<Poruka> = async (data) => {
     try {
@@ -65,7 +74,7 @@ const KontaktFroma = () => {
       // Handle the error
     }
 
-    await new Promise((resolve) => setTimeout(resolve, 5000))
+    await new Promise((resolve) => setTimeout(resolve, 300))
   }
 
   // SERVER ACTIONS (NOT WORKING WITH NETLIFY)
@@ -89,120 +98,130 @@ const KontaktFroma = () => {
     }
   }
 
+  const { rive, RiveComponent } = useRive({
+    src: "/rive/sendIcon.riv",
+    stateMachines: "Fly",
+    autoplay: true,
+  })
+
+  const isFlyingInput = useStateMachineInput(rive, "Fly", "isFlying")
+
   return (
-    <form
-      className="flex flex-col gap-6 has-[:focus]:outline-none [&_input]:outline-none [&_textarea]:outline-none"
-      onSubmit={handleSubmit(clientSendEmailAPI)}
-    >
-      <div className="felx felx-col relative">
-        <input
-          {...imeRest}
-          ref={(element) => {
-            imeRef.current = element
-            imeRefRHF(element)
-          }}
-          type="name"
-          name="ime"
-          className="peer w-full border-b-4 border-b-green-dark bg-transparent  pb-1 placeholder-transparent focus:outline-none "
-          id="ime"
-          placeholder="Ime i Prezime..."
-        />
-        {errors.ime && (
-          <p className="mt-1 text-red-500">{errors.ime.message}</p>
-        )}
-        <label
-          htmlFor="ime"
-          className="absolute -top-7 left-0 text-sm font-semibold transition-all duration-75 ease-out hover:cursor-pointer peer-placeholder-shown:-top-2 peer-placeholder-shown:text-base peer-focus:-top-7 peer-focus:text-sm"
-          onClick={() => setFocus("ime")}
-        >
-          IME I PREZIME
-        </label>
-      </div>
-      <div className="felx felx-col relative mt-8">
-        <input
-          {...emailRest}
-          ref={(element) => {
-            emailRef.current = element
-            emailRefRHF(element)
-          }}
-          type="email"
-          name="email"
-          placeholder="Email..."
-          className="peer w-full border-b-4 border-b-green-dark bg-transparent  pb-1 placeholder-transparent focus:outline-none"
-          id="email"
-        />
-        {errors.email && (
-          <p className="mt-1 text-red-500">{errors.email.message}</p>
-        )}
-        <label
-          htmlFor="email"
-          className="absolute -top-7 left-0 text-sm font-semibold transition-all duration-75 ease-out hover:cursor-pointer peer-placeholder-shown:-top-2 peer-placeholder-shown:text-base peer-focus:-top-7 peer-focus:text-sm"
-          onClick={() => setFocus("email")}
-        >
-          EMAIL
-        </label>
-      </div>
-      <div className="felx felx-col relative mt-8">
-        <input
-          {...telRest}
-          ref={(element) => {
-            telRef.current = element
-            telRefRHF(element)
-          }}
-          type="tel"
-          name="tel"
-          placeholder="Kontakt broj..."
-          className="peer w-full border-b-4 border-b-green-dark bg-transparent  pb-1 placeholder-transparent focus:outline-none"
-          id="tel"
-        />
-        {errors.tel && (
-          <p className="mt-1 text-red-500">{errors.tel.message}</p>
-        )}
-        <label
-          htmlFor="tel"
-          className="absolute -top-7 left-0 text-sm font-semibold transition-all duration-75 ease-out hover:cursor-pointer peer-placeholder-shown:-top-2 peer-placeholder-shown:text-base peer-focus:-top-7 peer-focus:text-sm"
-          onClick={() => setFocus("tel")}
-        >
-          KONTAKT TELEFON
-        </label>
-      </div>
-      <div className="felx felx-col relative mt-8">
-        <label
-          htmlFor="poruka"
-          className=" relative -top-2 cursor-pointer font-semibold"
-          onClick={() => setFocus("poruka")}
-        >
-          PORUKA
-        </label>
-        <textarea
-          {...porukaRest}
-          ref={(element) => {
-            porukaRef.current = element
-            porukaRefRHF(element)
-          }}
-          className="mt-1 h-32 w-full rounded-lg border-2 border-dashed border-green-light bg-transparent p-4 placeholder:text-green-light"
-          id="poruka"
-          name="poruka"
-          placeholder="Upišite svoju poruku ovdje..."
-        />
-        {errors.poruka && (
-          <p className="mt-1 text-red-500">{errors.poruka.message}</p>
-        )}
-      </div>
-      <button
-        type="submit"
-        className={`flex items-center justify-center rounded-lg bg-green-light p-2 px-3 py-3 text-lg font-extrabold text-white hover:bg-green-light-hover hover:shadow-lg focus:bg-green-light-hover ${
-          isSubmitting ? "cursor-wait bg-green-light-hover" : ""
-        }`}
-        disabled={isSubmitting}
+    <div>
+      <form
+        className="flex flex-col gap-6 has-[:focus]:outline-none [&_input]:outline-none [&_textarea]:outline-none"
+        onSubmit={handleSubmit(clientSendEmailAPI)}
       >
-        {isSubmitting ? "..." : "Pošalji Poruku"}
-        <SendIcon className="ml-2 w-6" />
-      </button>
-      {errors.root && (
-        <p className="mt-1 text-red-500">{errors.root.message}</p>
-      )}
-    </form>
+        <div className="felx felx-col relative">
+          <input
+            {...imeRest}
+            ref={(element) => {
+              imeRef.current = element
+              imeRefRHF(element)
+            }}
+            type="name"
+            name="ime"
+            className="peer w-full border-b-4 border-b-green-dark bg-transparent  pb-1 placeholder-transparent focus:outline-none "
+            id="ime"
+            placeholder="Ime i Prezime..."
+          />
+          {errors.ime && (
+            <p className="mt-1 text-red-500">{errors.ime.message}</p>
+          )}
+          <label
+            htmlFor="ime"
+            className="absolute -top-7 left-0 text-sm font-semibold transition-all duration-75 ease-out hover:cursor-pointer peer-placeholder-shown:-top-2 peer-placeholder-shown:text-base peer-focus:-top-7 peer-focus:text-sm"
+            onClick={() => setFocus("ime")}
+          >
+            IME I PREZIME
+          </label>
+        </div>
+        <div className="felx felx-col relative mt-8">
+          <input
+            {...emailRest}
+            ref={(element) => {
+              emailRef.current = element
+              emailRefRHF(element)
+            }}
+            type="email"
+            name="email"
+            placeholder="Email..."
+            className="peer w-full border-b-4 border-b-green-dark bg-transparent  pb-1 placeholder-transparent focus:outline-none"
+            id="email"
+          />
+          {errors.email && (
+            <p className="mt-1 text-red-500">{errors.email.message}</p>
+          )}
+          <label
+            htmlFor="email"
+            className="absolute -top-7 left-0 text-sm font-semibold transition-all duration-75 ease-out hover:cursor-pointer peer-placeholder-shown:-top-2 peer-placeholder-shown:text-base peer-focus:-top-7 peer-focus:text-sm"
+            onClick={() => setFocus("email")}
+          >
+            EMAIL
+          </label>
+        </div>
+        <div className="felx felx-col relative mt-8">
+          <input
+            {...telRest}
+            ref={(element) => {
+              telRef.current = element
+              telRefRHF(element)
+            }}
+            type="tel"
+            name="tel"
+            placeholder="Kontakt broj..."
+            className="peer w-full border-b-4 border-b-green-dark bg-transparent  pb-1 placeholder-transparent focus:outline-none"
+            id="tel"
+          />
+          {errors.tel && (
+            <p className="mt-1 text-red-500">{errors.tel.message}</p>
+          )}
+          <label
+            htmlFor="tel"
+            className="absolute -top-7 left-0 text-sm font-semibold transition-all duration-75 ease-out hover:cursor-pointer peer-placeholder-shown:-top-2 peer-placeholder-shown:text-base peer-focus:-top-7 peer-focus:text-sm"
+            onClick={() => setFocus("tel")}
+          >
+            KONTAKT TELEFON
+          </label>
+        </div>
+        <div className="felx felx-col relative mt-8">
+          <label
+            htmlFor="poruka"
+            className=" relative -top-2 cursor-pointer font-semibold"
+            onClick={() => setFocus("poruka")}
+          >
+            PORUKA
+          </label>
+          <textarea
+            {...porukaRest}
+            ref={(element) => {
+              porukaRef.current = element
+              porukaRefRHF(element)
+            }}
+            className="mt-1 h-32 w-full rounded-lg border-2 border-dashed border-green-light bg-transparent p-4 placeholder:text-green-light"
+            id="poruka"
+            name="poruka"
+            placeholder="Upišite svoju poruku ovdje..."
+          />
+          {errors.poruka && (
+            <p className="mt-1 text-red-500">{errors.poruka.message}</p>
+          )}
+        </div>
+        <button
+          type="submit"
+          className={`flex items-center justify-center rounded-lg bg-green-light p-2 px-3 py-3 text-lg font-extrabold text-white hover:bg-green-light-hover hover:shadow-lg focus:bg-green-light-hover ${
+            isSubmitting ? "cursor-wait bg-green-light-hover" : ""
+          }`}
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "" : "Pošalji Poruku"}
+          <RiveComponent className="h-8 w-8" />
+        </button>
+        {errors.root && (
+          <p className="mt-1 text-red-500">{errors.root.message}</p>
+        )}
+      </form>
+    </div>
   )
 }
 
