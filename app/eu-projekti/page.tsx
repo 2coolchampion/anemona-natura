cc0eb5a0e543c596bec2223f01674cb80ae57ee4
2coolchampion <franvidicek@gmail.com>
2024-02-26 12:44:54 +0100

    Add copy to EU Projekti page

--- app/eu-projekti/page.tsx
+++ app/eu-projekti/page.tsx
@@ -3,8 +3,33 @@ import MainWrapper from "@/components/ui/main"
 const EUProjekti = () => {
   return (
     <MainWrapper>
-      <section className="mt-48 text-center font-bold">
-        EU Projekti... Work in progress
+      <section className="mt-48 font-bold">
+        <div className="flex flex-col bg-gray-300">
+          <h1>
+            EU PROJEKTI PODMJERA 8.5.1. - KONVERZIJA DEGRADIRANIH ŠUMSKIH
+            SASTOJINA I ŠUMSKIH KULTURA
+          </h1>
+          <p>
+            klijentima pružamo uslugu organizacije, savjetovanja i pripreme
+            projekata iz Europskih fondova. Specijalizirani smo u izradi
+            projekata za podmjeru 8.5.1. — konverzija degradiranih šumskih
+            sastojina i šumskih kultura. Pripremamo i izrađujemo cjelokupnu
+            dokumentaciju za potrebe projekta iz Europskih strukturnih i
+            investicijskih fondova.
+          </p>
+          <p>
+            Prihvatljivi korisnici su šumoposjednici u skladu sa člankom 14.
+            Zakona o šumama („Narodne novine", br. 68/2018, 115/2018, 98/2019,
+            32/2020 i 145/20) te udruge šumoposjednika.{" "}
+          </p>
+          <p>
+            Cilj pokretanja projekta je dodjela sredstava za prevođenje
+            degradiranih šumskih sastojina, šumskih sastojina narušene strukture
+            po vrstama drveća i šumskih kultura u,mješovite sastojine autohtonih
+            vrsta drveća visokog uzgojnog oblika, a naš tim iza sebe ima brojne
+            uspješne i ostvarene projekte koje možete pogledati u nastavku.
+          </p>
+        </div>
       </section>
     </MainWrapper>
   )
