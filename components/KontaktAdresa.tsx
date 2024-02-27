65178f96e136b25bb16a46de6c40de2c33282fe4
2coolchampion <franvidicek@gmail.com>
2024-02-27 16:17:56 +0100

    formatting and remove VL in KontaktAdresa

--- components/KontaktAdresa.tsx
+++ components/KontaktAdresa.tsx
@@ -4,16 +4,16 @@ const KontaktAdresa = () => {
   return (
     <div className="mt-16 flex flex-col items-start justify-start space-y-4 text-background-light">
       <div className="flex  w-full items-start gap-4 p-4 sm:px-0">
-        <PersonIcon className="lg:text-green-super-light h-8 w-8" />
+        <PersonIcon className="h-8 w-8 lg:text-green-super-light" />
 
-        <div className="lg:text-green-super-light lg:text-lg lg:font-bold">
-          VL. Sabrina Hribar
+        <div className="lg:text-lg lg:font-bold lg:text-green-super-light">
+          Sabrina Hribar
         </div>
       </div>
       <div className="flex w-full items-start gap-4 p-4 sm:px-0">
-        <PinIcon className="lg:text-green-super-light h-9 self-start" />
+        <PinIcon className="h-9 self-start lg:text-green-super-light" />
         <div>
-          <h3 className="text-green-super-light hidden text-lg font-semibold lg:block">
+          <h3 className="hidden text-lg font-semibold text-green-super-light lg:block">
             ADRESA
           </h3>
           Anemona Natura d.o.o. <br /> M.J. Šporera 20 <br /> 47 000 Karlovac,
@@ -21,9 +21,9 @@ const KontaktAdresa = () => {
         </div>
       </div>
       <div className="flex w-full items-center gap-4 p-4 sm:px-0">
-        <PhoneIcon className="lg:text-green-super-light h-8 w-8 self-start" />
+        <PhoneIcon className="h-8 w-8 self-start lg:text-green-super-light" />
         <div className=" flex flex-col">
-          <h3 className="text-green-super-light hidden text-lg font-semibold lg:block">
+          <h3 className="hidden text-lg font-semibold text-green-super-light lg:block">
             BROJ TELEFONA
           </h3>
           <a href="tel:%2B385992868188" className="underline">
@@ -32,9 +32,9 @@ const KontaktAdresa = () => {
         </div>
       </div>
       <div className="flex w-full items-center gap-4 p-4 sm:px-0">
-        <MailIcon className="lg:text-green-super-light w-8 self-start" />
+        <MailIcon className="w-8 self-start lg:text-green-super-light" />
         <div className="flex flex-col">
-          <h3 className="text-green-super-light hidden text-lg font-semibold lg:block">
+          <h3 className="hidden text-lg font-semibold text-green-super-light lg:block">
             EMAIL
           </h3>
           <a href="mailto:anemona.natura.info@gmail.com" className="underline">
