69fea712ce06d536036a10a500b8f8b726e024c5
2coolchampion <franvidicek@gmail.com>
2024-03-01 13:12:03 +0100

    remove extra whitespace in ToastSucess.tsx

--- components/toast/ToastSuccess.tsx
+++ components/toast/ToastSuccess.tsx
@@ -4,7 +4,7 @@ const ToastSuccess = ({ message }: { message: string }) => {
   return (
     <div className="flex items-center rounded-md border-2 border-[#C7F9DA] border-opacity-25 bg-green-light-hover px-6 py-4 text-[#C7F9DA] shadow-xl">
       {" "}
-      <Check className="mr-2 w-5 " />
+      <Check className="mr-2 w-5" />
       {message}
     </div>
   )
