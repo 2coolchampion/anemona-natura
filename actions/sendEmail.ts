4ef3076a0733a67069186a31ac8f135a02976f1f
Fran Vidicek <franvidicek@gmail.com>
2025-01-14 14:10:06 +0100

    Add a comment to sendEmail server function

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,6 +5,8 @@ import { Resend } from "resend"
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
+// This is never used in the actual code because it's a server-side function and at the time I didn't manage to make it work with netlify.
+
 const sendEmail = async (porukaData: unknown) => {
   // server-side validation
   const result = porukaSchema.safeParse(porukaData)
